951a86aca7c93877575916df1e89cba36e6fdd2c
anharsaja <anharmuk12@gmail.com>
2024-12-01 00:44:08 +0000

    feat: change date format

--- app/Models/Consultation.php
+++ app/Models/Consultation.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Consultation extends Model
 {
@@ -14,6 +15,20 @@ class Consultation extends Model
     protected $fillable = [
         'mahasiswa_id', 'course_id', 'dosen_id', 'issue', 'status', 'reason_rejected', 'scheduled_time', 'note'
     ];
+
+    public function setScheduledTimeAttribute($value)
+    {
+        // Menggunakan Carbon untuk memformat hanya tanggalnya
+        $this->attributes['scheduled_time'] = Carbon::parse($value)->toDateString(); // Format menjadi 'Y-m-d'
+    }
+
+    /**
+     * (Optional) Accessor jika ingin mengembalikan format yang lebih user-friendly saat ditampilkan.
+     */
+    public function getScheduledTimeAttribute($value)
+    {
+        return Carbon::parse($value)->format('Y-m-d'); // Menampilkan 'Y-m-d'
+    }
 
     /**
      * Relasi ke tabel User (mahasiswa).
