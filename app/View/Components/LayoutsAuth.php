2beff8e0a4cf51099f6e254e9728fffbd8ff7f4a
anharsaja <anharmuk12@gmail.com>
2024-12-04 18:48:57 +0000

    feat: auth error fixed

--- app/View/Components/LayoutsAuth.php
+++ app/View/Components/LayoutsAuth.php
@@ -21,6 +21,6 @@ class LayoutsAuth extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('layouts.auth-layouts');
+        return view('components.layouts-auth');
     }
 }
